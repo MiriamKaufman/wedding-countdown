6f2944748cdd8968d805f2b30a15b277a0ed3e44
miriamkau <miriamkau@elad.co.il>
2026-03-23 15:18:54 +0200

    Revert next.config.mjs to original state

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/countdown.svg',
-        destination: '/api/countdown',
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
