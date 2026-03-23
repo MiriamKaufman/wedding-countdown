4831225813e3f8d5c3d47c147affd43d6a170160
miriamkau <miriamkau@elad.co.il>
2026-03-23 15:10:06 +0200

    Add rewrite rule to serve API as SVG file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/countdown.svg',
+        destination: '/api/countdown',
+      },
+    ];
+  },
+};
 
 export default nextConfig;
